7b3a3990fc8e61d09f8c411c82d502a874d7b447
JumpmanFR <jumpman@jumpman.fr>
2022-03-28 00:39:55 +0200

    Relocated stuff in file selection for longer text

--- ebb.asm
+++ ebb.asm
@@ -1,1 +1,8 @@
 org $5F010; incbin font.bin
+
+org $33EC0; incbin window_file_select_new.bin
+org $33885; db $B0,$66
+org $33889; db $CE,$66
+org $3388D; db $EC,$66
+org $33EDC; db $07,$67
+org $33EFA; db $07,$67
