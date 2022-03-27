23fe2dfd2dc74df5fc8fbb74d2602c8914e2a907
JumpmanFR <jumpman@jumpman.fr>
2022-03-28 01:11:57 +0200

    Cleaned up last change with variables for better clarity

--- ebb.asm
+++ ebb.asm
@@ -1,8 +1,15 @@
-org $5F010; incbin font.bin
+define addr_main_font $5F010
+org {addr_main_font}; incbin font.bin
 
-org $33EC0; incbin window_file_select_new.bin
-org $33885; db $B0,$66
-org $33889; db $CE,$66
-org $3388D; db $EC,$66
-org $33EDC; db $07,$67
-org $33EFA; db $07,$67
+define base_file_select $2D810
+define addr_win_new_game $33EC0
+define size_slot $1E
+define offset_ptr_start_up $1C
+define offset_start_up $1B
+org {addr_win_new_game}; incbin window_file_select_new.bin 			// new window arrangement with more room
+org $33885; dw {addr_win_new_game}-{base_file_select}				// pointer to slot 1
+org $33889; dw {addr_win_new_game}-{base_file_select}+{size_slot} 	// pointer to slot 2
+org $3388D; dw {addr_win_new_game}-{base_file_select}+{size_slot}*2	// pointer to slot 3
+// Pointers to "Start up" IN the bin file
+org {addr_win_new_game}+{offset_ptr_start_up}; dw {addr_win_new_game}-{base_file_select}+{size_slot}*2+{offset_start_up}
+org {addr_win_new_game}+{offset_ptr_start_up}+{size_slot}; dw {addr_win_new_game}-{base_file_select}+{size_slot}*2+{offset_start_up}
