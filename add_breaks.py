749647f9f98ea1fe30049fe241b6e7c48229cab6
JumpmanFR <jumpman@jumpman.fr>
2023-01-08 23:48:59 +0100

    More dynamic add_breaks.py (different line types)

--- add_breaks.py
+++ add_breaks.py
@@ -2,21 +2,108 @@ import re
 import textwrap
 import sys
 
-ROW_MAX_LENGTH = 23
+LINE_TYPES = {
+    "NORMAL_TEXT": {
+        "max_length": 22,
+        "leading_spaces": " ",
+        "max_lines": 3,
+        "extra_last_lines": 1
+    },
+    "SHORTER_TEXT": {
+        "max_length": 19,
+        "leading_spaces": " ",
+        "max_lines": 3,
+        "extra_last_lines": 1
+    },    
+    "SHORTER_QUESTION": {
+        "max_length": 19,
+        "leading_spaces": " ",
+        "max_lines": 3,
+        "extra_last_lines": 0
+    },
+    "ITEM_DESCRIPTION": {
+        "max_length": 22,
+        "leading_spaces": " ",
+        "max_lines": 4,
+        "extra_last_lines": 0
+    },
+    "NORMAL_ITEM": {
+        "max_length": 12,
+        "leading_spaces": "",
+        "max_lines": 1,
+        "extra_last_lines": 0
+    },
+    "NORMAL_ITEM": {
+        "max_length": 12,
+        "leading_spaces": "",
+        "max_lines": 1,
+        "extra_last_lines": 0
+    },
+    "SHORTER_ITEM": {
+        "max_length": 11,
+        "leading_spaces": "",
+        "max_lines": 1,
+        "extra_last_lines": 0
+    },
+    "ADDITIONAL_TEXT": {
+        "max_length": 23,
+        "leading_spaces": " ",
+        "max_lines": 3,
+        "extra_last_lines": 1
+    },
+    "ENEMY_DESCRIPTION": {
+        "max_length": 26,
+        "leading_spaces": "",
+        "max_lines": 3,
+        "extra_last_lines": 0
+    },
+    "BATTLE": {
+        "max_length": 28,
+        "leading_spaces": "",
+        "max_lines": 3,
+        "extra_last_lines": 0
+    },
+    "ACTION": {
+        "max_length": 27,
+        "leading_spaces": "",
+        "max_lines": 3,
+        "extra_last_lines": 0
+    },
+    "CREDITS": {
+        "max_length": 28,
+        "leading_spaces": "  ",
+        "max_lines": 16,
+        "extra_last_lines": 0
+    },
+    "EPILOGUE": {
+        "max_length": 28,
+        "leading_spaces": "",
+        "max_lines": 4,
+        "extra_last_lines": 0
+    }
+}
 
 def computeLength(str):
     return len(str)
 
-def handleReplic(str):
-    str = str.replace("[SBREAK]","")
+def handleReplic(str, lineType):
+    str = str.replace("[SBREAK]" + lineType["leading_spaces"], " ")
+    str = str.replace("[SBREAK]", "")
     
-    segmentsArr = str.split("[PAUSE]")
+    # replic: one line in script.txt
+    # segment: between two PAUSEs
+    # parag: between two hard BREAKs
+    # row: between two BREAKs
 
-    for i,segmentStr in enumerate(segmentsArr):
+    segmentsArr = str.split("[PAUSE]")
+    
+    for i, segmentStr in enumerate(segmentsArr):
+        
+        isLastSegment = (i == len(segmentsArr) - 1)
 
         paragsArr = segmentStr.split("[HBREAK]")
 
-        for j,paragStr in enumerate(paragsArr):
+        for j, paragStr in enumerate(paragsArr):
 
             wordsArr = re.split(r'\s(?:(?=(?:(?![\]\)}]).)*[\[{])|(?!.*[\]}]))', paragStr)
             #wordsArr = re.split(" ", paragStr)
@@ -24,19 +111,31 @@ def handleReplic(str):
             newStr = wordsArr[0]
             rowLength = computeLength(newStr)
 
+            #rowCount = 0
+
             for wordStr in wordsArr[1:]:
                 wordLength = computeLength(wordStr)
 
-                if (rowLength + 1 + wordLength > ROW_MAX_LENGTH):
+                if (rowLength + 1 + wordLength > lineType["max_length"]):
+                    #rowCount += 1;
                     newStr += "[BREAK]"
-                    rowLength = 0
-
-                newStr += " " + wordStr
-                rowLength += 1 + wordLength
+                    #if (rowCount >= lineType["max_lines"]):
+                    #    newStr += "[PAUSE]"
+                    #    rowCount = 0
+                    newStr += lineType["leading_spaces"]
+                    rowLength = len(lineType["leading_spaces"])
+                    newStr += wordStr
+                    rowLength += wordLength
+                else:
+                    newStr += " " + wordStr
+                    rowLength += 1 + wordLength
 
             paragsArr[j] = newStr
 
         segmentsArr[i] = "[BREAK]".join(paragsArr)
+        
+        # Now to add PAUSEs
+        
 
     return "[PAUSE]".join(segmentsArr)
 
@@ -52,9 +151,12 @@ inputFile = open(sys.argv[1],"r",encoding="ISO-8859-1")
 outputFile = open(sys.argv[2],"w",encoding="ISO-8859-1")
 lines = inputFile.readlines()
 for line in lines:
-    match = re.match("^([0-9A-F]{3}-E: )(.*)$", line)
-    if match:
-        outputFile.write(match.group(1) + handleReplic(match.group(2)) + '\n')
+    replicMatch = re.match("^([0-9A-F]{3}-E: )(.*)$", line)
+    if replicMatch:
+        outputFile.write(replicMatch.group(1) + handleReplic(replicMatch.group(2), curLineType) + '\n')
     else:
+        typeMatch = re.match("^//\s*#TYPE=(.*)$", line)
+        if (typeMatch):
+            curLineType = LINE_TYPES[typeMatch.group(1)]
         outputFile.write(line)
 
